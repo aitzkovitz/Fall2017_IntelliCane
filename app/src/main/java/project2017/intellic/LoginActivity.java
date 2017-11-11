71db0840a524a77aab2ff749fbb93f3122f7f9ac
yuxin8 <zhangyuxin4123671@gmail.com>
2017-11-11 15:06:15 -0600

    delete the select login page, fixed the password showing issue

--- app/src/main/java/project2017/intellic/LoginActivity.java
+++ app/src/main/java/project2017/intellic/LoginActivity.java
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_select_login);
+        setContentView(R.layout.activity_login);
 
         firebaseAuth = FirebaseAuth.getInstance();
     }
